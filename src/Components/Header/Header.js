8d85771eb50de1e77dbef7673126ba2a21e4bc8a
Mathias <Mathias70250@gmail.com>
2022-02-20 22:40:39 +0100

    first-commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+
+import "./header.css";
+
+function Headers() {
+  return (
+    <div className="header">
+      
+      <button className="header-login">Connexion</button>
+      
+      </div>
+  );
+}
+
+export default Headers;
---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,17 @@
+import './menu.css';
+
+function Menu () {
+    return (
+    <div className="menu-nav">
+    <h1 className="nav-tuto">Tutoriel</h1>
+    <h1 className="nav-forum">Forum</h1>
+    <h1 className="nav-buy">Achat Kamas</h1>
+    <h1 className="nav-send">Vente Kamas</h1>
+    <h1 className="nav-contact">Contact</h1>
+    </div>
+    
+    ) 
+}
+
+
+export default Menu;
