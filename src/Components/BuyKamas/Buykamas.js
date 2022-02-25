18adf4ba79a78dddbebea565dce3f4067714be0d
Mathias <Mathias70250@gmail.com>
2022-02-25 19:30:43 +0100

    data-ok

--- src/Components/BuyKamas/Buykamas.js
+++ src/Components/BuyKamas/Buykamas.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import "./buykama.css";
+import React from "react";
+import serveur from "../../data/data";
 
 export default function Buykamas() {
   return (
-    <div>
+    <div className="description-buy">
       <h1>Achat de kamas !</h1>
-      <p>Choix du serveur de jeu</p>
+      <label className="choose-serv">
+        Choix du serveur:
+        <select className="list-serv" name="serveur" value="">
+         
+          {
+          serveur.map((elem) => (
+            <option key="">{elem.name}</option>
+          ))
+          }
+
+          
+        </select>
+      </label>
     </div>
-  )
+  );
 }
---
+++ src/data/data.js
@@ -0,0 +1,44 @@
+const serveur = [
+  
+    
+    
+      
+        {
+          serveur_id:1,
+          name:'ush', 
+          kamas:1000000,
+          stock: 50, 
+
+        },
+        {
+          serveur_id:2,
+          name:'otomai', 
+          img_path:'./images/vivre/poisson.png', 
+          kamas:1000000,
+          stock: 50, 
+        },
+        {
+          serveur_id:3,
+          name:'Julith', 
+          kamas:1000000,
+          stock: 50, 
+        },
+        {
+          serveur_id:4,
+          name:'Agride', 
+          kamas:1000000,
+          stock: 50, 
+        },
+        {
+          serveur_id:5,
+          name:'Nidas', 
+          kamas:1000000,
+          stock: 50, 
+        },
+        
+      
+     
+  
+];
+
+export default serveur;
